30fba541f5a0c89ec70d897c915132bf1f74bcb1
Steven Haryanto (on PC, Jakarta) <stevenharyanto@gmail.com>
2014-07-10 15:27:56 +0700

    Initial commit (not yet implemented)

---
+++ lib/Perinci/CmdLine/Common.pm
@@ -0,0 +1,13 @@
+package Perinci::CmdLine::Common;
+
+use 5.010001;
+use strict;
+use warnings;
+#use Log::Any '$log';
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Routines common to Perinci::CmdLine{,::Lite}
+
